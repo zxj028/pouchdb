dadd71b813e0fa486d4f9c94561f32e051644ecb
Nolan Lawson <nolan.lawson@gmail.com>
2014-07-07 16:34:57 -0700

    new directory

--- bin/run-cordova.sh
+++ bin/run-cordova.sh
@@ -83,6 +83,6 @@ if [[ $(cordova plugin list | grep sqlite) ]]; then
   cordova plugin rm com.phonegap.plugins.sqlite
 fi
 if [[ $SQLITE_PLUGIN == 'true' ]]; then 
-  cordova plugin add /Users/nolan/workspace/Cordova-SQLitePlugin/plugin
+  cordova plugin add /Users/nolan/workspace/Cordova-SQLitePlugin
 fi
 ../../node_modules/cordova/bin/cordova $ACTION $CLIENT
